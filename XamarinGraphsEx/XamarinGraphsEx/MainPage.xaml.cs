4591727fb09f788e16be88039db8541e6033cee6
Алиса Карпенко <adkarpenko@edu.hse.ru>
2020-05-10 04:34:38 +0300

    Добавьте файлы проекта.

---
+++ XamarinGraphsEx/XamarinGraphsEx/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App13.ViewModels;
+
+namespace App13
+{
+	public partial class MainPage : ContentPage
+	{
+        MainPageViewModel vm;
+		public MainPage()
+		{
+            vm = new MainPageViewModel();
+			InitializeComponent();
+            this.BindingContext = vm;
+		}
+
+        private async void btnPieCharts_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new PieChart());
+        }
+    }
+}
